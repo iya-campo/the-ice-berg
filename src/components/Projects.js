808f1676b62d49775ba12cbfd50796e88fcf3ecb
IyaCampo <ilyannacampo@gmail.com>
2021-02-03 14:53:23 +0800

    Fixing images bug

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
       <div className='w-100'>
         <h1 className='text-center'>My Work</h1>
         <h4 className='text-center'>Here's a few samples of my work.</h4>
-        <Flickity className={'carousel'} elementType={'div'} options={flickityOptions} disableImagesLoaded={false} reloadOnUpdate static={true}>
+        <Flickity className={'carousel'} elementType={'div'} options={flickityOptions} disableImagesLoaded={false} reloadOnUpdate static>
           {/* images are approx. 600x880 */}
           <img src='images/web-samples/react-1.png' alt='' />
           <img src='images/web-samples/react-2.png' alt='' />
